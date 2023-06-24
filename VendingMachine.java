de7b4ffde375da0f922769553d9fb52aeca4f95d
Emmanoks <137618930+Emmanoks@users.noreply.github.com>
2023-06-24 21:04:02 +0800

    Create VendingMachine.java

---
+++ VendingMachine.java
@@ -0,0 +1,48 @@
+public class VendingMachine {
+
+    private Slot[] slots;
+    private CashInventory cashInventory;
+    private ItemInventory itemInventory;
+    private TransactionLog transactionLog;
+
+    public void VendingMachine()
+    {
+
+    }
+
+    public void restockItems()
+    {
+
+    }
+
+    public void setItemPrice(int slot, double price)
+    {
+
+    }
+
+    public void insertCoin(Coin coin)
+    {
+
+    }
+
+    public void selectItems(int slot)
+    {
+
+    }
+
+    public void produceChange()
+    {
+
+    }
+
+    public void cancelTransaction()
+    {
+
+    }
+
+    String getTransactionSummary()
+    {
+        
+    }
+    
+}
