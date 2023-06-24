d92b27e58295bd08dc9234742b057d79be62dc2a
Emmanoks <137618930+Emmanoks@users.noreply.github.com>
2023-06-24 21:35:34 +0800

    betlog

---
+++ Slot.java
@@ -0,0 +1,25 @@
+public class Slot {
+
+    private Item item;
+    private int quantity;
+
+    public Slot(Item item, int quantity)
+    {
+
+    }
+
+    public Item getItem()
+    {
+
+    }
+
+    public int getQuantity()
+    {
+
+    }
+
+    public void decreaseQuantity()
+    {
+        
+    }
+}
